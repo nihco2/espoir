305bac714bdb798cd65dba8fcd8971c090575e8f
unknown <nihco2@gmail.com>
2015-07-19 23:32:59 +0200

    Start Route job

--- scripts/app.jsx
+++ scripts/app.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import Player from './components/player.jsx';
+import Homepage from './components/homepage.jsx';
+import Router from 'react-router';
 
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
 window.React = React;
 
-React.render(<Player />, document.getElementById('content'));
+var routes = ( < Route handler = {
+	App
+} >
+< Route path = "player"
+handler = {
+	Player
+}
+/> > < DefaultRoute handler = {
+Homepage
+}
+/> < /Route >
+
+);
+
+var App = React.createClass({
+	render() {
+		return ( < Homepage / > )
+	}
+});
+
+Router.run(routes, (Root) => {
+	React.render( < Root / > , document.body);
+});
+//React.render( < Player / > , document.getElementById('content'));
---
+++ scripts/components/homepage.jsx
@@ -0,0 +1,24 @@
+var React = require('react');
+var texts = require('../../assets/texts.json');
+import ReactRouter from 'react-router';
+
+var Link = ReactRouter.Link;
+
+let Homepage = React.createClass({
+render() {
+	return ( < div className = "homepage" >
+		< h1 > {
+			texts.h1
+		} < /h1> < h2 > {
+		texts.h2
+	} < /h2> < h3 > {
+	texts.h3
+} < /h3> < h4 > {
+texts.h4
+} < /h4>  < p > {
+texts.description
+} < /p> < /div > );
+}
+});
+
+export default Homepage;
