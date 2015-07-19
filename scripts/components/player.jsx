4382541e3026c5ac0badfaf7887bf185d42b7c1b
unknown <nihco2@gmail.com>
2015-07-19 17:03:13 +0200

    Customize player

--- scripts/components/player.jsx
+++ scripts/components/player.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var $ = require('jquery');
 var assets = require('../../assets/assets.json');
 
 let Player = React.createClass({
@@ -15,91 +16,108 @@ let Player = React.createClass({
 		return this.getVideo().volume;
 	},
 
-	getVideo(something) {
+	getVideo() {
 		return document.getElementById('video');
 	},
 
-	getCurrentTime: function () {
-		this.getVideo().currentTime;
+	handleClickPause: function () {
+		if (!this.getVideo().paused) {
+			this.getVideo().pause();
+			$('.play').show();
+		} else {
+			this.getVideo().play();
+			$('.play').hide();
+		}
 	},
 
-	play: function () {
+	handleClickPlay: function (event) {
+		event.stopPropagation();
 		this.getVideo().play();
+		$('.play').hide();
 	},
 
-	pause: function () {
-		this.getVideo().pause();
-	},
+	handleProgressBarMouseDown: function (e) {
 
-	metaDataLoaded: function () {
-		console.log('metaDataLoaded', document.getElementById('video').duration)
-	},
+		if (!this.getVideo().paused) {
+			this.getVideo().pause();
+		}
 
-	timeUpdate: function ($ignore) {
-
+		// Position x de la souris lors du clic
+		var x = e.pageX - $('.progress').offset().left - 50;
 		var progWidth = document.querySelector('.progress').offsetWidth;
+		// Mise à jour du temps actuel
+		var currentTime = (x / progWidth) * this.getDuration();
 
-		// Le temps actuel de la vidéo, basé sur la barre de progression
-		var time = Math.round((document.querySelector('.progress-bar').offsetWidth / progWidth) * this.getDuration());
-
-		// Le temps "réel" de la vidéo
-		var curTime = this.getCurrentTime();
-
-		// Les secondes sont initialisées à 0 par défaut, les minutes correspondent à la durée divisée par 60
-		// tminutes et tseconds sont les minutes et secondes totales
-		var seconds = 0,
-			minutes = Math.floor(time / 60),
-			tminutes = Math.round(this.getDuration() / 60),
-			tseconds = Math.round((this.getDuration()) - (tminutes * 60));
-		console.log(document.querySelector('.progress-bar').offsetWidth, this.getDuration(), this.getBuffered())
-			// Si le temps existe (enfin, la durée de la vidéo !)
-		if (time) {
-			// Les secondes valent la durée moins les minutes
-			seconds = Math.round(time) - (60 * minutes);
-
-			// Si nous avons plus de 59 secondes
-			if (seconds > 59) {
-				// On augmente les minutes et on soustrait les secondes en trop
-				seconds = Math.round(time) - (60 * minutes);
-				if (seconds == 60) {
-					minutes = Math.round(time / 60);
-					seconds = 0;
-				}
-			}
-
-		}
-
-		// Mise à jour de la barre de progression
-		var updProgWidth = (curTime / this.getDuration()) * progWidth
-
-		// Ajout des zéros initiaux pour les valeurs inférieures à 10
-		if (seconds < 10) {
-			seconds = '0' + seconds;
-		}
-		if (tseconds < 10) {
-			tseconds = '0' + tseconds;
-		}
-
-		// Une variable que nous verrons plus tard
-		if ($ignore != true) {
-			document.querySelector('.progress-bar').style.width = updProgWidth + 'px';
-			document.querySelector('.progress-button').style.width = (updProgWidth - document.querySelector('.progress-button').offsetWidth) + 'px';
-		}
-
-		// Ajustement des durées
-		document.querySelector('.ctime').innerHTML = (minutes + ':' + seconds);
-		document.querySelector('.ttime').innerHTML = (tminutes + ':' + tseconds);
-
-		// En mode lecture, mise à jour des valeurs du tampon
-		if (this.getVideo().currentTime > 0 && this.getVideo().paused == false && this.getVideo().ended == false) {
-			//bufferLength();
-		}
-
+		this.getVideo().currentTime = currentTime;
 	},
+
 	componentDidMount: function () {
-		this.getVideo().addEventListener('timeupdate', this.timeUpdate());
-		this.getVideo().addEventListener('loadedmetadata', this.metaDataLoaded());
-		//this.play();
+		var self = this;
+		window.addEventListener('load', function () {
+			self.getVideo().addEventListener('loadedmetadata', function () {
+				self.getVideo().addEventListener('timeupdate', function () {
+					var progWidth = document.querySelector('.progress').offsetWidth - 50;
+
+					// Le temps actuel de la vidéo, basé sur la barre de progression
+					var time = Math.round((document.querySelector('.progress-bar').offsetWidth / progWidth) * self.getDuration());
+
+					// Le temps "réel" de la vidéo
+					var curTime = self.getVideo().currentTime;
+
+					// Les secondes sont initialisées à 0 par défaut, les minutes correspondent à la durée divisée par 60
+					// tminutes et tseconds sont les minutes et secondes totales
+					var seconds = 0,
+						minutes = Math.floor(self.getVideo().currentTime / 60), //Math.floor(time / 60),
+						tminutes = Math.round(self.getDuration() / 60),
+						tseconds = Math.round((self.getDuration()) - (tminutes * 60));
+
+					// Si le temps existe (enfin, la durée de la vidéo !)
+					if (time) {
+						// Les secondes valent la durée moins les minutes
+						seconds = Math.floor(self.getVideo().currentTime) - (60 * minutes);
+
+						// Si nous avons plus de 59 secondes
+						if (seconds > 59) {
+							// On augmente les minutes et on soustrait les secondes en trop
+							seconds = Math.round(time) - (60 * minutes);
+							if (seconds == 60) {
+								minutes = Math.round(time / 60);
+								seconds = 0;
+							}
+						}
+
+					}
+
+					// Mise à jour de la barre de progression
+					var updProgWidth = (curTime / self.getDuration()) * progWidth
+
+					// Ajout des zéros initiaux pour les valeurs inférieures à 10
+					if (seconds < 10) {
+						seconds = '0' + seconds;
+					}
+					if (minutes < 10) {
+						minutes = '0' + minutes;
+					}
+					if (tseconds < 10) {
+						tseconds = '0' + tseconds;
+					}
+
+
+					//document.querySelector('.progress-bar').style.width = updProgWidth + 'px';
+					document.querySelector('.progress-button').style.left = updProgWidth + 'px';
+
+
+					// Ajustement des durées
+					document.querySelector('.ctime').innerHTML = (minutes + ':' + seconds);
+					document.querySelector('.ttime').innerHTML = (tminutes + ':' + tseconds);
+
+					// En mode lecture, mise à jour des valeurs du tampon
+					if (self.getVideo().currentTime > 0 && self.getVideo().paused == false && self.getVideo().ended == false) {
+						//bufferLength();
+					}
+				});
+			});
+		});
 	},
 	render() {
 		let video = assets.video,
@@ -109,15 +127,23 @@ let Player = React.createClass({
 			< video id = "video"
 			poster = {
 				poster
-			} >
+			}
+			preload = "metadata" >
 			< source src = {
 				video
 			}
+
 			type = "video/mp4" / >
 			< source src = "movie-hd.mp4"
 			type = "video/mp4" / >
-			< /video> < div className = "player" > < div className = "play" > < /div > < div className = "progress" > < div className = "progress-bar" >
-			< div className = "button-holder" >
+			< /video> < div className = "player" onClick={this.handleClickPause}>  < div className = "play"
+			onClick = {
+				this.handleClickPlay
+			} > < /div >  < div className = "progress" > < div className = "progress-bar"
+			onMouseDown = {
+				this.handleProgressBarMouseDown
+			} >
+			< div className = "mask" > < /div> < div className = "button-holder" >
 			< div className = "progress-button" > < /div> < /div > ' < /div> < div className = "time" > < span className = "ctime" > 00: 00 < /span> < span className = "ttime" > 00: 00 < /span > < /div> < /div > < div className = "volume" > < /div> < /div > < /div>
 		);
 	}
